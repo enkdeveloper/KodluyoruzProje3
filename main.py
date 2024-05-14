03a733a12bf582a253eb840cd87b50119cacfe70
elifnazkarakiz@outlook.com <Elifnazkarakiz@outlook.com>
2024-05-14 23:01:28 +0300

    Proje 3 [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree
    Proje 3
    [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
    
    Örnek: root x'dir. root'un sağından y bulunur. Solunda z bulunur vb.

---
+++ main.py
@@ -0,0 +1,38 @@
+[7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+Aşamalar
+Root oluşturma:
+İlk eleman root olarak seçilir.
+Root 7'dir.
+
+Elemanları ekleme:
+5: 5, 7'den küçüktür. Soluna yerleştirilir.
+Root 7'dir. Solunda 5 bulunur.
+1: 1, 7'den ve 5'ten küçüktür. 5'in soluna yerleştirilir.
+5'in solunda 1 bulunur.
+8: 8, 7'den büyüktür. Sağında yer alır.
+Root 7'dir. Sağında 8 bulunur.
+3: 3, 7'den ve 5'ten küçüktür, 1'den büyüktür. 1'in sağına yerleştirilir.
+1'in sağında 3 bulunur.
+6: 6, 7'den küçüktür ve 5'ten büyüktür. 5'in sağına yerleştirilir.
+5'in sağında 6 bulunur.
+0: 0, 7'den, 5'ten ve 1'den küçüktür. 1'in soluna yerleştirilir.
+1'in solunda 0 bulunur.
+9: 9, 7'den ve 8'den büyüktür. 8'in sağına yerleştirilir.
+8'in sağında 9 bulunur.
+4: 4, 7'den ve 5'ten küçüktür, 1'den büyüktür ve 3'ten büyüktür. 3'ün sağına yerleştirilir.
+3'ün sağında 4 bulunur.
+2: 2, 7'den ve 5'ten küçüktür, 1'den büyüktür ve 3'ten küçüktür. 3'ün soluna yerleştirilir.
+3'ün solunda 2 bulunur.
+
+<--------------------------->
+Root 7'dir.
+Root'un solunda 5 bulunur.
+Root'un sağında 8 bulunur.
+5'in solunda 1 bulunur.
+5'in sağında 6 bulunur.
+1'in solunda 0 bulunur.
+1'in sağında 3 bulunur.
+8'in sağında 9 bulunur.
+3'ün solunda 2 bulunur.
+3'ün sağında 4 bulunur.
